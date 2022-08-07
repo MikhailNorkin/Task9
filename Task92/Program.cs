bd0d6d67d510a1d745eac60faa907c001f3cb7f5
Mikhail <mvnorkin@gmail.com>
2022-08-07 23:56:54 +0300

    task92 is complite

---
+++ Task92/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int m = 0, n = 0, sum = 0;
+m = int.Parse(Console.ReadLine());
+n = int.Parse(Console.ReadLine());
+Console.Write($"M = {m}; N = {n} -> ");
+for (int i = m; i <= n; i++)
+{
+    sum = sum + i;
+    if (i == n)
+    {
+        Console.WriteLine($"{sum}");
+    }
+}

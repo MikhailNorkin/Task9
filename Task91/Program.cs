c69aec5aa69bb21dc650c72cfdc8f7a30ace6b06
Mikhail <mvnorkin@gmail.com>
2022-08-07 22:35:31 +0300

    Task91 is complite

--- Task91/Program.cs
+++ Task91/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+int m = 0, n = 0;
+m = int.Parse(Console.ReadLine());
+n = int.Parse(Console.ReadLine());
+Console.Write($"M = {m}; N = {n}. -> ");
+for (int i = m; i <= n; i++)
+{
+    if (i != n)
+    {
+        Console.Write($"{i},");
+    }
+    else
+    {
+        Console.WriteLine($"{i}");
+    }    
+}
